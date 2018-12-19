5a2e18bc15740503ab06b8a3b574464a3f20bd2e
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-18 17:39:14 -0700

    Update navbar spec

--- spec/features/nav_bar_links_spec.rb
+++ spec/features/nav_bar_links_spec.rb
@@ -19,4 +19,29 @@ describe 'Nav Bar' do
       expect(page).to have_no_content('Trips')
     end
   end
+
+  context 'As a user' do
+    let(:user) { create(:user) }
+
+    it 'navigates user' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit '/'
+
+      click_on 'About'
+      expect(current_path).to eq('/about')
+
+      click_on 'ParKween'
+      expect(current_path).to eq('/')
+
+      click_on 'New Trip'
+      expect(current_path).to eq('/trips/new')
+
+      click_on 'Trips'
+      expect(current_path).to eq('/trips')
+
+      click_on 'Logout'
+
+      expect(current_path).to eq('/')
+    end
+  end
 end
