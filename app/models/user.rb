66ec8253be989cdfc3e000ed3d391ebb5ee1efca
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-12-19 15:19:07 -0700

    fix multiple same user issue with omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,18 @@
 class User < ApplicationRecord
   validates_presence_of     :google_client_id,
                             :role
+  validates_uniqueness_of :google_client_id
   validates_numericality_of :role
 
   def self.find_or_create_from_auth_hash(auth)
-		where(google_client_id: auth[:uid]).first_or_initialize.tap do |user|
-      user = User.new
-      user.role = 0
-      # user.provider = auth.provider
-			user.google_client_id = auth["uid"]
-			user.first_name = auth["info"]["first_name"]
-			user.last_name = auth["info"]["last_name"]
-			user.email = auth["info"]["email"]
-      user.token = auth["info"]["token"]
-      user.refresh_token = auth.info.refresh_token
-			user.save!
-		end
-	end
+    user = find_or_create_by(google_client_id: auth["uid"])
+    user.role = 0
+  	user.first_name = auth["info"]["first_name"]
+  	user.last_name = auth["info"]["last_name"]
+  	user.email = auth["info"]["email"]
+    user.token = auth["credentials"]["token"]
+    # user.refresh_token = auth.info.refresh_token
+  	user.save
+    user
+  end
 end
--- spec/features/user_logs_in_via_google_ominauth_spec.rb
+++ spec/features/user_logs_in_via_google_ominauth_spec.rb
@@ -7,13 +7,12 @@ RSpec.feature "user logs in" do
 
     auth = stub_omniauth
 
-    User.find_or_create_from_auth_hash(auth)
+    user = User.find_or_create_from_auth_hash(auth)
 
-    new_user = User.first
 
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit root_path
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(new_user)
 
     expect(page).to have_link("Logout")
 
