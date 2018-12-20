d16402b7324e1478bfab54c5d142005b15aad2d1
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-20 08:57:12 -0700

    Travis config attempt 4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,14 +96,14 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.javascript_driver = :chrome
-
-Capybara.configure do |config|
-  config.default_max_wait_time = 10 # seconds
-  config.default_driver        = :selenium
-end
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
+#
+# Capybara.javascript_driver = :chrome
+#
+# Capybara.configure do |config|
+#   config.default_max_wait_time = 10 # seconds
+#   config.default_driver        = :selenium
+# end
 end
