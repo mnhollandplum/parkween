645797fe30c9ecd9126e0f30ff3ac9250cec0476
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-19 22:06:33 -0700

    Creates a google service

---
+++ app/services/google_service.rb
@@ -0,0 +1,15 @@
+class GoogleService
+
+  private
+  def conn
+    Faraday.new(url:"https://maps.googleapis.com") do |faraday|
+      faraday.params["key"] = ENV['GOOGLE_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)[:results]
+  end
+
+end
