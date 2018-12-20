72d919e21d61cbfb55e1026b27256827320218d6
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-19 23:13:29 -0700

    Add a google geolocation service.

---
+++ app/services/google_geolocation_service.rb
@@ -0,0 +1,17 @@
+class GoogleCoordinateService
+
+  def get_geolocation_coordinates
+    get_json("/geolocation/v1/geolocate?")[:location]
+  end
+  private
+  def conn
+    Faraday.new(url:"https://www.googleapis.com") do |faraday|
+      faraday.params["key"] = ENV['GOOGLE_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
+end
