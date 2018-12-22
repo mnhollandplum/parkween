cbbc6d85062987ee9b73e40ae7bbbecbfd034943
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-22 14:27:00 -0700

    Modify parkwhis service

--- app/services/parkwhiz_results_service.rb
+++ app/services/parkwhiz_results_service.rb
@@ -1,17 +1,19 @@
 class ParkwhizResultsService
 
   def get_results(search_lat, search_lng)
-    get_json("/search/json?key=#{ENV['PARKWHIZ_API_KEY']}&lat=#{search_lat}&lng=#{search_lng}&sort=distance")
+    get_json("/search/?key=#{ENV['PARKWHIZ_API_KEY']}&lat=#{search_lat}&lng=#{search_lng}&sort=distance")[:parking_listings]
   end
+
   private
+
   def conn
-    Faraday.new(url:"http://api.parkwhiz.com/search/json?key=d1ec881e8951e9bfaed1efbc4b0ed84daac67303&lat=39.7507834&lng=-104.9964355&sort=distance") do |faraday|
+    Faraday.new(url:"https://api.parkwhiz.com") do |faraday|
       faraday.params["key"] = ENV['PARKWHIZ_API_KEY']
       faraday.adapter Faraday.default_adapter
     end
   end
+  
   def get_json(url)
-    require "pry"; binding.pry
     JSON.parse(conn.get(url).body, symbolize_names: true)
   end
 end
--- spec/services/parkwhiz_results_service_spec.rb
+++ spec/services/parkwhiz_results_service_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 describe ParkwhizResultsService do
   it 'exists' do
-    # VCR.use_cassette("parkwhiz_results") do
+    VCR.use_cassette("parkwhiz_results") do
     service = ParkwhizResultsService.new
     expect(service).to be_a(ParkwhizResultsService)
-    # end
+    end
   end
-  it 'returns coordinates' do
-    # VCR.use_cassette("parkwhiz_results") do
+  it 'returns parking_spaces' do
+    VCR.use_cassette("parkwhiz_results") do
       service = ParkwhizResultsService.new
-      expect(service.get_results(39.7507834, -104.9964355)).to eq(pizza)
-    # end
+      expect(service.get_results(39.7507834, -104.9964355)).to be_an_instance_of(Array)
+    end
   end
 end
