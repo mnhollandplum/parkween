dd1602597617fc537384047ebae5778349b190bd
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-23 13:31:49 -0700

    Create Lot class

---
+++ app/models/lot.rb
@@ -0,0 +1,19 @@
+class Lot
+
+  attr_reader :lat,
+              :lng,
+              :name,
+              :address,
+              :price,
+              :distance
+
+  def initialize(lat, lng, name, address, price, distance)
+    @lat = lat
+    @lng = lng
+    @name = name
+    @address = address
+    @price = price
+    @distance = distance
+  end
+
+end
