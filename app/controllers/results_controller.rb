75c5084bbec6a61c821e41da200620db0b156bb5
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-23 13:30:58 -0700

    Update results controller
    Updated results controller to create and pass facade for index

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,8 +1,17 @@
 class ResultsController < ApplicationController
   def create
-    redirect_to results_path
+    # Assuming params[:destination] is a real address
+    cordinates = GoogleCoordinateService.new.get_coordinates(params[:destination])
+    lat = cordinates[:lat]
+    lng = cordinates[:lng]
+    radius = params[:radius]
+    address = params[:location]
+
+    @facade = ResultsFacade.new(lat, lng, address, radius)
+    render :index
   end
 
   def index
+    @facade = ResultsFacade.new(lat, lng, address, radius)
   end
 end
