8e7de792f4a57f9adb963fda1cb2e8dcff856b0f
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-26 10:46:19 -0600

    Remove pry

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,14 +1,14 @@
 class ResultsController < ApplicationController
   def create
     # Assuming params[:destination] is a real address
-    coordinates = GoogleCoordinateService.new.get_coordinates(params[:destination])
-    lat = coordinates[:lat]
-    lng = coordinates[:lng]
+    @coordinates = GoogleCoordinateService.new.get_coordinates(params[:destination])
+    
+    lat = @coordinates[:lat]
+    lng = @coordinates[:lng]
     radius = params[:radius]
     address = params[:location]
 
     @facade = ResultsFacade.new(lat, lng, address, radius)
-    binding.pry
     render :index
   end
 
