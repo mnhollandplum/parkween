82af136cb3f5a78b1065c515a46d37dc314b1bff
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-26 15:26:46 -0700

    Build meter service
    Meter service pulls data directly from a CSV, but is built to interact 
    with the facade as if it were a API service

---
+++ app/services/meter_service.rb
@@ -0,0 +1,13 @@
+require 'csv'
+class MeterService
+
+  def get_results(lat, lng, radius)
+    meter_data = File.read("./lib/data/parking_meters.csv")
+    CSV.parse(meter_data, headers: true).find_all do |meter|
+      search_lat = meter["LATITUDE"].to_f
+      search_lng = meter["LONGITUDE"].to_f
+      meter["DISTANCE"] = Geocoder::Calculations.distance_between([lat,lng],[search_lat, search_lng])
+      radius.to_f <= meter["DISTANCE"]
+    end
+  end
+end
