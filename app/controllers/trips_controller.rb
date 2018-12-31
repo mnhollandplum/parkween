95af9a1e1444d2f3520a99193fb0c30b3eb39e0e
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-31 16:58:04 -0700

    Create new saved trip in trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -13,5 +13,9 @@ class TripsController < ApplicationController
   end
 
   def create
+    destination = params[:destination_address]
+    chosen_spot = params[:location]
+    Trip.create(final_address: destination, chosen_spot: chosen_spot, user_id: current_user.id)
+    redirect_to trips_path
   end
 end
